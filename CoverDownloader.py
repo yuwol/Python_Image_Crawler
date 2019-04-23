d63c8cb58c0023ea4d8983c0f90b650225e8d4dc
yuwol <yuwol@users.noreply.github.com>
2019-04-24 00:10:57 +1000

    Update CoverDownloader.py

--- CoverDownloader.py
+++ CoverDownloader.py
@@ -12,41 +12,26 @@ def search(dirname):
         for filename in filenames:
             full_filename = os.path.join(dirname, filename)
             if os.path.isdir(full_filename):
-                #print(filename + ' ' + dirname)                
-                #if filename != "새 폴더" and dirname != os.getcwd():
-                #if filename != "새 폴더" :
                 print(full_filename)
-                search(full_filename)
+                #search(full_filename)
             else:
-                print(full_filename + " - 이동 ")
-                term = os.path.splitext(filename)[0]
-                print(term + " - 이동 ")
-                s = requests.Session()
-
-                with requests.Session() as s:
-                    downloadURL = 'http://video-jav.net/wp-content/uploads/image-' + term + '.jpg'
-                    print(downloadURL)
-                    #download(downloadURL, os.path.join(os.getcwd(), term + '.jpg'))
-
-                    import urllib.request
-                    try:
-                        urllib.request.urlretrieve(downloadURL, os.path.join(os.getcwd(), term + '.jpg')) 
-                    except urllib.error.HTTPError as e:
-                        print(e.__dict__)
-                    except urllib.error.URLError as e:
-                        print(e.__dict__)
-                if not dirname == os.getcwd():              
-                    ext = os.path.splitext(full_filename)[-1]
-                    videos = ['.mp4','.avi','.mpg','.mpeg']
-                    images = ['.jpg','.png','.jpeg']
-                    if ext in videos: 
-                        fsize = os.path.getsize(full_filename)
-                        if fsize > 1000000000:
-                            print(full_filename + " - 이동 ")
-                            #shutil.move(full_filename, os.getcwd())
-                    elif ext in images:
-                        print(full_filename + " - 이미지 이동 ")
-                        #shutil.move(full_filename, os.path.join(os.getcwd(), filename))
+                ext = os.path.splitext(full_filename)[-1]
+                videos = ['.mp4','.avi','.mpg','.mpeg']
+                if ext in videos: 
+                    term = os.path.splitext(filename)[0]
+                    if(os.path.exists(os.path.join(os.getcwd(), term.upper() + '.jpg'))):
+                        print(term.upper() + ' - exists')
+                    else:
+                        downloadURL = 'http://video-jav.net/wp-content/uploads/image-' + term + '.jpg'
+                        print(downloadURL)
+                        #download(downloadURL, os.path.join(os.getcwd(), term + '.jpg'))
+                        import urllib.request
+                        try:
+                            urllib.request.urlretrieve(downloadURL, os.path.join(os.getcwd(), term + '.jpg')) 
+                        except urllib.error.HTTPError as e:
+                            print(e.__dict__)
+                        except urllib.error.URLError as e:
+                            print(e.__dict__)
     except PermissionError:
         pass
 
